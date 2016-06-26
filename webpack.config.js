71c91d496bd9bc1a818030193912b6757a1272e0
Ayyaaz <aafzal-khan@neworbit.co.uk>
2016-06-26 11:31:43 +0100

    sassified, i think

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var autoprefixer = require('autoprefixer');
 
 var exclusionRegexs = [
     /node_modules/
@@ -8,12 +10,15 @@ module.exports = {
     context: __dirname,
     output: {
         path: path.resolve(__dirname, './dist'),
-        filename: 'datepicker.js',
+        filename: '[name]',
         library: 'bootstrapDatepicker',
         libraryTarget: 'umd'
     },
 
-    entry: path.resolve(__dirname, './src/index.js'),
+    entry: {
+        'datepicker.js': path.resolve(__dirname, './src/index.js'),
+        'datepicker.css': path.resolve(__dirname, './src/datepicker.scss')
+    },
 
     //stats: 'minimal',
 
@@ -30,7 +35,17 @@ module.exports = {
                 test: /\.js$/,
                 loader: 'ng-annotate!babel',
                 exclude: exclusionRegexs
+            },
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract('style', 'css?sourceMap!postcss-loader?sourceMap!sass?sourceMap')
             }
         ]
-    }
+    },
+
+    postcss: [ autoprefixer({ browsers: ['last 2 versions'] }) ],
+
+    plugins: [
+        new ExtractTextPlugin('/style.css')
+    ]
 };
